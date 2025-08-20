d03c8d41cbf89d2753517984be8ee70b71a25cb7
Woon <blankw100@gmail.com>
2025-08-20 08:41:11 +0800

    title only

---
+++ ExD_crypt.py
@@ -0,0 +1,6 @@
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("ExDcrypt")
+print(ascii_banner)
+print("Welcome to ExDcrypt, the encryption and decryption tool for ExD files.")
+input("Press Enter to exit...")
